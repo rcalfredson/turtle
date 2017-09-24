dac19936332a644138cb84243fed72023f18e2bb
Robert C. Alfredson <robert.c.alfredson@ibm.com>
2017-09-24 08:16:29 -0400

    Add new function to detect four-week breakouts.

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
-!function(){"use strict";window.addEventListener("DOMContentLoaded",function(){console.log("Hello World")})}();
+!function(){"use strict";window.addEventListener("DOMContentLoaded",function(){console.log("Hello World"),fetch("/getFourWeekBreakout?sym=RAD").then(function(response){return response.json()}).then(function(json){console.log(json)})})}();
 //# sourceMappingURL=maps/app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,11 @@
 
   window.addEventListener('DOMContentLoaded', function appDCL() {
     console.log('Hello World'); // eslint-disable-line no-console
+    fetch('/getFourWeekBreakout?sym=RAD').then(function (response) { // eslint-disable-line func-names
+      
+      return response.json();
+    }).then(function (json) { // eslint-disable-line func-names
+      console.log(json); // eslint-disable-line no-console
+    });
   });
 }());
