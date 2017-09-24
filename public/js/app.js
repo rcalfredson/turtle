7054054ee44c2a1f483a54e65f37810bc25fc370
Robert C. Alfredson <robert.c.alfredson@ibm.com>
2017-09-24 08:56:11 -0400

    Removed "public" folder

--- public/js/app.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";const renderFourWeekBreakout=function(symbol){fetch("/detectFourWeekBreakout?sym="+symbol).then(function(response){return response.json()}).then(function(json){console.log(json)})};window.addEventListener("DOMContentLoaded",function(){console.log("Hello World"),renderFourWeekBreakout("RAD")})}();
-//# sourceMappingURL=maps/app.js.map
